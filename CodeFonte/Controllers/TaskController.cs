f0a8bb4aebfe1a51febb039915f7aab147ae34db
bankmarqs <danilojmarqs@gmail.com>
2024-07-29 12:42:27 -0400

    Adicionando o Controller

---
+++ CodeFonte/Controllers/TaskController.cs
@@ -0,0 +1,43 @@
+using TaskManager.Models;
+using TaskManager.Services;
+
+namespace TaskManager.Controllers
+{
+    public class TaskController
+    {
+        private readonly TaskService _taskService;
+
+        public TaskController(TaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        public void AddTask(string title, string description, DateTime dueDate, int priority)
+        {
+            _taskService.AddTask(title, description, dueDate, priority);
+        }
+
+        public void RemoveTask(int id)
+        {
+            _taskService.RemoveTask(id);
+        }
+
+        public void UpdateTask(int id, string title, string description, DateTime dueDate, int priority)
+        {
+            _taskService.UpdateTask(id, title, description, dueDate, priority);
+        }
+
+        public Task GetTaskById(int id)
+        {
+            return _taskService.GetTaskById(id);
+        }
+
+        public IEnumerable<Task> GetAllTasks()
+        {
+            return _taskService.GetAllTasks();
+        }
+    }
+}
+
+
+//Essa classe, creio que é uma das mais importantes, ela faz a ligação entre a apresentação e a camada de serviço, basicamente expôe os metódos do CRUD, delegando a lógica de negócios para a TaskService.
