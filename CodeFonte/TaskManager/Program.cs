1c679df3b523b2baa762e358d2008f2f08151478
bankmarqs <danilojmarqs@gmail.com>
2024-07-29 14:44:39 -0400

    Atualizando o Arq. Program

--- CodeFonte/TaskManager/Program.cs
+++ CodeFonte/TaskManager/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TaskManager.Controllers;
+using TaskManager.Models;
+using TaskManager.Services;
+using TaskManager.Views;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ITaskRepository taskRepository = new InMemoryTaskRepository();
+        TaskService taskService = new TaskService(taskRepository);
+        TaskController taskController = new TaskController(taskService);
+        ConsoleView consoleView = new ConsoleView(taskController);
+
+        consoleView.DisplayMenu();
+    }
+}
+
+
+//Esse é ponto de entrada dessa aplicação toda, vai configurar as dependências e inicia a interface do console. 
+//Quando executado o método "main" é chamado e a interface do console é exibida.
