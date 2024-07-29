ef62f9bc653f9a031d209ad68479333b75cac178
bankmarqs <danilojmarqs@gmail.com>
2024-07-29 12:49:38 -0400

    Adicionando o Programa

---
+++ CodeFonte/Program/Program.cs
@@ -0,0 +1,20 @@
+using TaskManager.Controllers;
+using TaskManager.Models;
+using TaskManager.Services;
+using TaskManager.Views;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ITaskRepository taskRepository = new InMemoryTaskRepository();
+        TaskService taskService = new TaskService(taskRepository);
+        TaskController taskController = new TaskController(taskService);
+        ConsoleView consoleView = new ConsoleView(taskController);
+
+        consoleView.DisplayMenu();
+    }
+}
+
+//Esse é ponto de entrada dessa aplicação toda, vai configurar as dependências e inicia a interface do console. 
+//Quando executado o método "main" é chamado e a interface do console é exibida.
